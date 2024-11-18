2c857575ff906eba420e1395d984860ab0feb4b2
Aditya Pant <pantanupam05@gmail.com>
2024-11-18 16:07:24 +0530

    self practice

--- src/statements.java
+++ src/statements.java
@@ -2,15 +2,17 @@ public class statements {
     public static void main(String[] args){
         System.out.println("Hello World");
 
-        int topScore = 80;
+        int topScore = 70;
         if (topScore < 100){
             System.out.println("you got the high score");
         }
 
-        int secondTopScore = 10;
+        int secondTopScore = 80;
         if(topScore > secondTopScore && topScore < 100){
             System.out.println("greater than the second top score and less than 100");
-
+        }
+        if (topScore < secondTopScore ){
+            System.out.println("the new top score is " + secondTopScore + " \n the old score was" + topScore );
         }
 
     }

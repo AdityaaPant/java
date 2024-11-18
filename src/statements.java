1339f8b06f72fc51f076e2bf419c359d9c39124f
Aditya Pant <pantanupam05@gmail.com>
2024-11-18 15:57:08 +0530

    logical and

--- src/statements.java
+++ src/statements.java
@@ -1,6 +1,17 @@
 public class statements {
     public static void main(String[] args){
         System.out.println("Hello World");
-        System.out.print("Hello World");
+
+        int topScore = 80;
+        if (topScore < 100){
+            System.out.println("you got the high score");
+        }
+
+        int secondTopScore = 10;
+        if(topScore > secondTopScore && topScore < 100){
+            System.out.println("greater than the second top score and less than 100");
+
+        }
+
     }
 }
